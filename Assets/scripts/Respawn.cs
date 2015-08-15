eae686c240d8a3f2101a55972aac6608e08db7d6
AaronTimms <aaronjtimms@hotmail.com>
2015-08-15 15:35:48 +0930

    Added Respawn.cs
    Implements basic respawning when leaving level bounds

---
+++ Assets/scripts/Respawn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+	
+	public int xLimit;
+	public int yLimit;
+	Vector2 home = new Vector2(0,0);
+	private Rigidbody2D rb2d;
+	//rb2d = GetComponent<Rigidbody2D>();
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if(CheckLimits()) 
+		{
+			transform.position = home;
+			rb2d.velocity = home;
+		}		
+
+	}
+
+	bool CheckLimits() 
+	{
+
+		if(transform.position.x<(xLimit*-1)||transform.position.x>xLimit)
+		{
+			return true;
+		}
+		if(transform.position.y<(yLimit*-1)||transform.position.y>yLimit)
+		{
+			return true;
+		}
+
+		return false;
+	}
+}
