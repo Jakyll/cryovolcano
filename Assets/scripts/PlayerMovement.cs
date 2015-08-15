00a31032b92f8e83e7e6ceee32e43e4b7a086a0b
Jakyll <Jack.leahy7@gmail.com>
2015-08-15 15:35:12 +0930

    New playermovement

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,37 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class player_controller : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
     [HideInInspector] public bool facingRight = true;
     [HideInInspector] public bool jump = false;
-    public float moveForce = 365f;
-    public float maxSpeed = 5f;
-    public float jumpForce = 1000f;
+
+    public float moveSpeed = 1F;
+    public float jumpForce = 1000F;
     public Transform groundCheck;
 
 
     private bool grounded = false;
-    //private Animator anim;
+    private Animator anim;
     private Rigidbody2D rb2d;
 
-
-    // Use this for initialization
     void Awake () 
     {
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
     }
     
-    // Update is called once per frame
     void Update () 
     {
         grounded = true;
 		//grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-		
-		//Vector3 pos = transform.position;
-		//pos.z = -10;
-		//Camera.main.transform.position = pos;
 		
         if (Input.GetButtonDown("Jump") && grounded)
         {
@@ -43,25 +36,36 @@ public class player_controller : MonoBehaviour {
     {
         float h = Input.GetAxis("Horizontal");
 
-        //anim.SetFloat("Speed", Mathf.Abs(h));
+        anim.SetFloat("Speed", Mathf.Abs(h));
+		
+		rb2d.velocity = new Vector2(moveSpeed * h, rb2d.velocity.y);
+		
+		if(jump)
+		{
+			rb2d.AddForce(new Vector2(0F, jumpForce));
+			jump = false;
+		}
 
-        if (h * rb2d.velocity.x < maxSpeed)
-            rb2d.AddForce(Vector2.right * h * moveForce);
+        //if (h * rb2d.velocity.x < maxSpeed)
+		//rb2d.AddForce(Vector2.right * h * moveForce);
 
-        if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-            rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        //if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+        //    rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 
         if (h > 0 && !facingRight)
+		{
             Flip ();
-        else if (h < 0 && facingRight)
+		}
+		else if (h < 0 && facingRight)
+		{
             Flip ();
-
-        if (jump)
-        {
+		}
+        //if (jump)
+        //{
             //anim.SetTrigger("Jump");
-            rb2d.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
-        }
+        //    rb2d.AddForce(new Vector2(0f, jumpForce));
+        //    jump = false;
+        //}
     }
 
 
